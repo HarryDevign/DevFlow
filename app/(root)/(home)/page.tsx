3f1ea62298f60f684ffa2052260cc92771d5bf3f
hoangle <hoangle@hiptechvn.com>
2024-02-26 14:13:56 +0700

    Creating Layouts using Next/Font and Metadata

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,10 @@
 import { UserButton } from "@clerk/nextjs";
+import Navbar from "../../../components/Navbar";
  
 export default function Home() {
   return (
     <div className="h-screen">
+      <Navbar/>
       <h1>Home</h1>
       <UserButton afterSignOutUrl="/"/>
     </div>
