75054e5b5ef20e0e6009f071e8e53960ddc1f3f9
hoangle <hoangle@hiptechvn.com>
2024-02-26 15:40:06 +0700

    Container and User Account

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,8 @@
 import { UserButton } from "@clerk/nextjs";
-import Navbar from "../../../components/Navbar";
  
 export default function Home() {
   return (
     <div className="h-screen">
-      <Navbar/>
-      <h1>Home</h1>
-      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
