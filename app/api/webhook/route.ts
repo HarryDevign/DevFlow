0edb022334e9f594b96d1e8b6d4f3fd1a915cb44
hoangle <hoangle@hiptechvn.com>
2024-03-25 20:52:43 +0700

    add api webhook to public route

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -55,6 +55,9 @@ export async function POST(req: Request) {
 
   const eventType = evt.type;
 
+  console.log({eventType});
+  
+
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
@@ -89,7 +92,7 @@ export async function POST(req: Request) {
   }
 
   if (eventType === "user.deleted") {
-    const { id } = evt.data
+    const { id } = evt.data;
 
     // const deleteUser = await deleteUser({
     //     clerkId: id!,
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/'],
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
- 
+
 export const config = {
   // Protects all routes, including api/trpc.
   // See https://clerk.com/docs/references/nextjs/auth-middleware
