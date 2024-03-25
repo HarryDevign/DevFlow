0321ede3d81312ab5d64b0ceb35c09e9b8d4852a
hoangle <hoangle@hiptechvn.com>
2024-03-25 10:37:28 +0700

    Implement Webhooks and User Actions

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -11,63 +11,58 @@ import Link from "next/link";
 export default async function Home() {
   const result = await getQuestions({});
 
-  console.log(result);
-
   return (
     <>
-      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center ">
-        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1> 
+
         <Link href="/ask-question" className="flex justify-end max-sm:w-full">
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
             Ask a Question
           </Button>
-        </Link>
-      </div>
+        </Link> 
+      </div> 
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        <LocalSearchbar
+        <LocalSearchbar 
           route="/"
           iconPosition="left"
-          placeholder="Search for questions..."
           imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
           otherClasses="flex-1"
         />
+
         <Filter
           filters={HomePageFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
-          containerClasses="hidden max-md:flex "
+          containerClasses="hidden max-md:flex"
         />
       </div>
+
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {result.questions.length > 0 ? (
-          result.questions.map((question) => {
-            return (
-              <QuestionCard
-                key={question._id}
-                _id={question._id}
-                title={question.title}
-                upvotes={question.upvotes}
-                tags={question.tags}
-                author={question.author}
-                views={question.views}
-                answers={question.answers}
-                createdAt={question.createdAt}
-              />
-            );
-          })
-        ) : (
-          <NoResult
+        {result.questions.length > 0 ?
+          result.questions.map((question) => (
+            <QuestionCard 
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+          : <NoResult 
             title="There’s no question to show"
-            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
-              discussion. our query could be the next big thing others learn from. Get
-              involved! 💡"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
-            linkTitle="Ask a question"
-          />
-        )}
+            linkTitle="Ask a Question"
+          />}
       </div>
     </>
-  );
+  )
 }
